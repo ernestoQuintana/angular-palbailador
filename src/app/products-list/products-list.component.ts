07c6edbd589b31517ea5047bb23154e2477f5429
Ernesto Quintana <ernesto.quintana.oliva@gmail.com>
2020-10-21 09:48:19 -0300

    agrega componentes,scss y pipes

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,4 +1,7 @@
+import { Xliff } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Product } from './products';
+//incluimos otros archivos.
 
 @Component({
   selector: 'app-products-list',
@@ -8,15 +11,45 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsListComponent implements OnInit {
 
   constructor() { }
-  product = {
+  //tipamos con Products
+  products: Product[] = [
+    {
       "nombre": "Ribens",
-      "tipo" : "pantalon",
-      "talle" : 42,
-      "precio" : 800,
-      "stock": 4,
-      "imagen": "assets/img/ribens.jpg"
-      }
-  
+      tipo: "pantalon",
+      talle: 42,
+      precio: 800,
+      stock: 4,
+      imagen: "assets/img/ribens.jpg",
+      clearance: false,
+    },
+    {
+      nombre: "Mario",
+      tipo: "Zapatos Hombre",
+      talle: 40,
+      precio: 2800,
+      stock: 3,
+      imagen: "assets/img/ribens.jpg",
+      clearance: false,
+    },
+    {
+      nombre: "Duqueza",
+      tipo: "Zapatos Femenino",
+      talle: 38,
+      precio: 1800,
+      stock: 2,
+      imagen: "assets/img/ribens.jpg",
+      clearance: false,
+    },
+    {
+      nombre: "Legacy",
+      tipo: "camisas",
+      talle: 12,
+      precio: 2000,
+      stock: 0,
+      imagen: "assets/img/ribens.jpg",
+      clearance: true,
+    }
+  ]
   ngOnInit(): void {
   }
 
