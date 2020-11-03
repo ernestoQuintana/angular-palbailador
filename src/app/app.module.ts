389312e4abcd0344f68d7e11354fe4238529ab54
Ernesto Quintana <ernesto.quintana.oliva@gmail.com>
2020-11-03 00:17:40 -0300

    agrega refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 
 import { FormsModule }from '@angular/forms';
+import { PalBailadorAboutComponent } from './pal-bailador-about/pal-bailador-about.component';
+import { PalBailadorCartComponent } from './pal-bailador-cart/pal-bailador-cart.component';
+import { PalBailadorProductsComponent } from './pal-bailador-products/pal-bailador-products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    PalBailadorAboutComponent,
+    PalBailadorCartComponent,
+    PalBailadorProductsComponent
   ],
   imports: [
     BrowserModule,
